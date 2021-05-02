3738f4537052a7ef3f59b23754a50ef1ab45cb05
silentbass24 <silentbass24@libero.it>
2021-05-02 17:07:57 +0200

    Correzione del file index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const {Router} = require('express'); // dal modulo express richiedo in importare
 const router = Router();
 
 router.get('/',(req,res)=>{
-    res.json({"Title":'Ciao Modo!! \nQusesto è il sever di yomel.'});
+    res.json({"Title":'Ciao Mondo!! \nQuesto è il server di Paolo.'});
 });
 
 module.exports=router;
