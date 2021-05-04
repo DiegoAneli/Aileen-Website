633a97956f5881b6601621f2d0a5392c1bf03640
YomelRivera <yomel@hotmail.it>
2021-05-04 13:25:43 +0200

    aggiunto le varie pagine

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,27 @@ router.get('/',(req,res)=>{
 
     //res.json({"Title":'Ciao Mondo!! \nQuesto è il server di Paolo.'});
     //res.sendFile(path.join(pathSRC,'views/index.html'));
-    res.render(pathSRC+'views/index.html');
+    res.render(pathSRC+'views/index.html',{title:"HOME"});
 });
 
-router.get('/prima.html',(req,res)=>{
-    res.render(pathSRC+'views/prima.html');
+router.get('/chi-sono.html',(req,res)=>{
+    res.render(pathSRC+'views/chi-sono.html',{title:"Chi sono?"});
+});
+
+router.get('/cosa-faccio.html',(req,res)=>{
+    res.render(pathSRC+'views/cosa-faccio.html',{title:"Cosa faccio"});
+});
+
+router.get('/collaboratori.html',(req,res)=>{
+    res.render(pathSRC+'views/collaboratori.html',{title:"Collaboratori"});
+});
+
+router.get('/informazioni-e-contatti.html',(req,res)=>{
+    res.render(pathSRC+'views/informazioni-e-contatti.html',{title:"Informazioni e contatti"});
+});
+
+router.get('/prove.html',(req,res)=>{
+    res.render(pathSRC+'views/prove.html',{title:"Prove"});
 });
 
 module.exports=router;
